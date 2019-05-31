c3a2a9ae800465e656cdee9a51b990ba764c80d3
Chieko Natori <chieko.natori@gmail.com>
2019-05-31 18:22:11 +1000

    Add and rewrite comments

--- src/route_model.cpp
+++ src/route_model.cpp
@@ -1,9 +1,15 @@
 #include "route_model.h"
 #include <iostream>
 
+//
+// This file defines RouteModel class which inherits Model class 
+// from IO2D example code.
+//
+
 RouteModel::RouteModel(const std::vector<std::byte> &xml) : 
 Model(xml) {
   int i = 0;
+  // Create RouteModel::Node objects that correspond to the nodes in the model.
   for (Model::Node cur_node : this->Nodes()) {
     Node new_node = Node(i, this, cur_node);
     m_Nodes.push_back(new_node);
@@ -12,13 +18,23 @@ Model(xml) {
   CreateNodeToRoadHashmap();
 }
 
+float RouteModel::Node::distance(Node to_node) const {
+  // Return the euclidean distance given by sqrt( (x1 - x2)^2 + (y1 - y2)^2 ).
+  return sqrt(pow(this->x - to_node.x, 2) + pow(this->y - to_node.y, 2));
+}
+
 void RouteModel::CreateNodeToRoadHashmap() {
 
   for(int i=0; i < Roads().size(); i++) {
     const Model::Road &road = Roads()[i];
 
     if(road.type != Model::Road::Type::Footway) {
+
+      // Add every node in the current road(=way) to node_to_road.
       for(int n : Ways()[road.way].nodes) {
+
+        // If this is the first time to find this node, set the empty vector of Road
+        //   with respect to this node in node_to_road.
         if(node_to_road.find(n) == node_to_road.end()) {
           node_to_road[n] = vector<const Model::Road*> {};
         }
@@ -35,6 +51,8 @@ RouteModel::Node* RouteModel::Node::FindNeighbor(vector<int> node_indices) {
   // Search the closest node to <this> from nodes of node_indices.
   for(int n_idx : node_indices) {
     Node &node = parent_model->SNodes()[n_idx];
+
+    // Find the closest node that hasn't been visited and that isn't itself.
     if( (!node.visited) && (node.distance(*this) != 0)) {
       if((closest_node == nullptr) ||
          (closest_node->distance(*this) > node.distance(*this))) {
@@ -52,8 +70,10 @@ void RouteModel::Node::FindNeighbors() {
   
   // Get the closest node on each road and store them in this->neighbors.
   for(const Model::Road* road : roads) {
+
     vector<int> nodes_on_road = parent_model->Ways()[road->way].nodes;
     RouteModel::Node* closest_on_road = FindNeighbor(nodes_on_road);
+
     if (closest_on_road != nullptr) {
       this->neighbors.push_back(closest_on_road);
     }
@@ -61,6 +81,7 @@ void RouteModel::Node::FindNeighbors() {
 }
 
 RouteModel::Node &RouteModel::FindClosestNode(float x, float y) {
+
   RouteModel::Node temp_node;
   temp_node.x = x;
   temp_node.y = y;
@@ -70,8 +91,12 @@ RouteModel::Node &RouteModel::FindClosestNode(float x, float y) {
   // Check the distance to every node in each road, and save the closest node.
   for(Model::Road road : Roads()) {
     if(road.type != Model::Road::Type::Footway) {
+      // Search only roads for vehicles.
+
+      // Loop over the node in the road and calculate the distance to (x,y).
       for(int n_idx : Ways()[road.way].nodes) {
         float dist = SNodes()[n_idx].distance(temp_node); 
+        // Save the closest node.
         if((closest_idx < 0) || (dist < min_dist)) {
           min_dist = dist;
           closest_idx = n_idx;
--- src/route_model.h
+++ src/route_model.h
@@ -9,39 +9,57 @@
 using std::vector;
 using std::unordered_map;
 
+//
+// This is a header file for RouteModel class.
+// RouteModel inherits Model class from IO2D example code.
+// Add attributes and methods which are used to perform A* search to find a shortest path.
+//
+
 class RouteModel : public Model {
 
   public:
+
+    // Define Node class which has some new attributes and methods for A* search. 
     class Node : public Model::Node {
+
       public:
-        // Add public Node variables and methods here.
-        Node * parent = nullptr;
+
+        Node* parent = nullptr; // Set the previous node in the path
       	float h_value = std::numeric_limits<float>::max();
       	float g_value = 0.0;
       	bool visited = false;
       	vector<Node *> neighbors;
+
         Node(){}
-        Node(int idx, RouteModel* search_model, Model::Node node) : Model::Node(node), parent_model(search_model), index(idx) {}
-      	float distance(Node to_node) const { return sqrt(pow(this->x - to_node.x, 2) + pow(this->y - to_node.y, 2)); }
-        void FindNeighbors();
+        Node(int idx, RouteModel* search_model, Model::Node node) : 
+			Model::Node(node), parent_model(search_model), index(idx) {}
+        
+      	float distance(Node to_node) const; // Get a distance from this node to to_node.
+        void FindNeighbors(); // Find candidates of the nearest neighbor.
+
       
       private:
-        // Add private Node variables and methods here.
-        int index;
-        RouteModel * parent_model = nullptr;
-        RouteModel::Node * FindNeighbor(vector<int> node_indices);        
+
+        int index; // arbitrary index for a node.
+        RouteModel* parent_model = nullptr; // The model this node belongs to.
+        // Find the closest neighbor from node_indices.
+        RouteModel::Node* FindNeighbor(vector<int> node_indices);
     };
     
-    // Add public RouteModel variables and methods here.
-    RouteModel(const vector<std::byte> &xml);  
     vector<Node> path; // This variable will eventually store the path that is found by the A* search.
-  	vector<Node> &SNodes() { return m_Nodes; }
+
+    RouteModel(const vector<std::byte> &xml);  
+
+    // Return Model::m_Nodes which stores all nodes in the Model.
+    vector<Node> &SNodes() { return m_Nodes; }
     unordered_map <int, vector<const Model::Road*>> &GetNodeToRoadMap() { return node_to_road; }
+    // Return the closest node from the coordinates (x,y).
     Node &FindClosestNode(float x, float y);
 
   private:
-    // Add private RouteModel variables and methods here.
-	vector<Node> m_Nodes;
+
+    vector<Node> m_Nodes;
+    // node_to_road stores a dictionary where each node refers to the list of road it belongs to.
     unordered_map <int, vector<const Model::Road*>> node_to_road;
     void CreateNodeToRoadHashmap();
 };
